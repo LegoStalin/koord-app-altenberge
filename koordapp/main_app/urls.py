175f89abdaeaad4e95e320572e99b1f837228c0f
Florian Luettgenau <fluettge@uni-muenster.de>
2023-11-28 20:59:36 +0100

    First Version of Master Overview Frame

--- koordapp/main_app/urls.py
+++ koordapp/main_app/urls.py
@@ -8,5 +8,6 @@ from .views import LoginInterfaceView
 urlpatterns = [ 
     path('', views.HomeView.as_view(), name='home'),
     path('login/', views.LoginInterfaceView.as_view(), name='login'),
-    path('create_activity/', CreateActivityView.as_view(), name='create_activity'),
+    path('create_activity/', views.CreateActivityView.as_view(), name='create_activity'),
+    path('master_web/', views.MasterHomeView.as_view(), name='master_web')
 ]
