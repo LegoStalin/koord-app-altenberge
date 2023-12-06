ac5fbca53d42aabec891fe478dbea07c479c934a
Yannick Wenger <y.wenger@gmx.de>
2023-12-06 21:41:10 +0100

    Finalisierung Docker

--- koordapp/main_app/apps.py
+++ koordapp/main_app/apps.py
@@ -5,16 +5,18 @@ import importlib
 
 class LogsystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main_app'    
+    name = 'main_app'
     def ready(self):
-
-        # Erstellung der Rechte Gruppen
-        models = importlib.import_module('django.contrib.auth.models')
-        if not models.Group.objects.filter(name='Admin').exists():
-            models.Group.objects.create(name='Admin')
-        if not models.Group.objects.filter(name='Gruppenleitung').exists():
-            models.Group.objects.create(name='Gruppenleitung')
-        if not models.Group.objects.filter(name='Raumbetreuer').exists():
-            models.Group.objects.create(name='Raumbetreuer')
-        if not models.Group.objects.filter(name='Ohne Rolle').exists():
-            models.Group.objects.create(name='Ohne Rolle')
+        try:
+            # Erstellung der Rechte Gruppen
+            models = importlib.import_module('django.contrib.auth.models')
+            if not models.Group.objects.filter(name='Admin').exists():
+                models.Group.objects.create(name='Admin')
+            if not models.Group.objects.filter(name='Gruppenleitung').exists():
+                models.Group.objects.create(name='Gruppenleitung')
+            if not models.Group.objects.filter(name='Raumbetreuer').exists():
+                models.Group.objects.create(name='Raumbetreuer')
+            if not models.Group.objects.filter(name='Ohne Rolle').exists():
+                models.Group.objects.create(name='Ohne Rolle')
+        except:
+            print('')
