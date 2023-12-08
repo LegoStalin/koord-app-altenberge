609dd2a6fcca19273d0ae01a2a41e9a455ede5ba
christiank119 <christian.kamann119@gmail.com>
2023-12-08 16:33:27 +0100

    Update models.py

--- koordapp/main_app/models.py
+++ koordapp/main_app/models.py
@@ -9,42 +9,32 @@ from django.contrib.auth.models import Group
 class Zeitraum(models.Model):
     startzeit = models.TimeField()
     endzeit = models.TimeField(null=True)
-
-
 # Datumsraum enhält zwei Tage mit konkreter Uhrzeit
 class Datumsraum(models.Model):
     startdatum = models.DateTimeField()
     enddatum = models.DateTimeField()
-
-
 class Nutzer(models.Model):
     vorname = models.CharField(max_length=100)
     nachname = models.CharField(max_length=100)
     # email = models.CharField(max_length=100)
-
-
 class Personal(models.Model):
     rolle = models.CharField(max_length=40)
-    rechte_gruppe = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)  # null=True rausmachen
+    rechte_gruppe = models.ForeignKey(Group, on_delete=models.CASCADE)
     nutzer = models.ForeignKey(Nutzer, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)  # null=True rausmachen
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     is_password_otp = models.BooleanField(default=True)
 
     class Meta:
         permissions = [("can_import", "Can import excel data")]
 
-
 class Raum(models.Model):
     raum_nr = models.SmallIntegerField()
     geschoss = models.CharField(max_length=4)
     kapazitaet = models.SmallIntegerField()
-
-
 class Gruppe(models.Model):
     name = models.CharField(max_length=50)
-    gruppenleiter_leiter = models.ForeignKey(Personal, on_delete=models.SET_NULL, null=True)  # ?
+    gruppenleiter_leiter = models.ForeignKey(Personal, on_delete=models.SET_NULL, null=True)        # ?
     raum = models.ForeignKey(Raum, on_delete=models.SET_NULL, null=True)
-
 
 class AGZeit(models.Model):
     class WOCHENTAG(models.TextChoices):
@@ -53,24 +43,22 @@ class AGZeit(models.Model):
         MITTWOCH = "Mittwoch"
         DONNERSTAG = "Donnerstag"
         FREITAG = "Freitag"
-
+    
     wochentag = models.TextField(choices=WOCHENTAG.choices, default=WOCHENTAG.MONTAG, max_length=10)
     zeitraum = models.ForeignKey(Zeitraum, on_delete=models.CASCADE)
-
 
 class AG(models.Model):
     name = models.CharField(max_length=50)
     beschreibung = models.CharField(max_length=500)
     max_anzahl = models.SmallIntegerField()
-    offene_AG = models.BooleanField()
-    leiter = models.ForeignKey(Personal, on_delete=models.CASCADE, null=True)  # null=True entfernen
-    angebots_datum_raum = models.ForeignKey(Datumsraum, on_delete=models.CASCADE, null=True)  # null=True entfernen
+    offene_AG = models.BooleanField() 
+    leiter = models.ForeignKey(Personal, on_delete=models.CASCADE)       
+    angebots_datum_raum = models.ForeignKey(Datumsraum, on_delete=models.CASCADE)
     ag_zeit = models.ManyToManyField(AGZeit)
 
-
 class Schueler(models.Model):
-    klasse = models.CharField(max_length=3)  # optional
-    tag_id = models.CharField(max_length=100)  # max_length abhängig von Tags id länge
+    klasse = models.CharField(max_length=3)             # optional
+    tag_id = models.CharField(max_length=100)          #max_length abhängig von Tags id länge
     bus_kind = models.BooleanField()
     name_eb = models.CharField(max_length=100)
     kontakt_eb = models.CharField(max_length=300)
@@ -78,8 +66,6 @@ class Schueler(models.Model):
     user_id = models.ForeignKey(Nutzer, on_delete=models.CASCADE)
     gruppen_id = models.ForeignKey(Gruppe, on_delete=models.CASCADE)
     ag_buchungen = models.ManyToManyField(AG)
-
-
 class Feedback(models.Model):
     class Feedbacks(models.TextChoices):
         GOOD = "GOOD"
@@ -89,18 +75,14 @@ class Feedback(models.Model):
     feedback_wert = models.CharField(choices=Feedbacks.choices, default=Feedbacks.MEDIUM, max_length=6)
     tag = models.DateField
     schueler_id = models.ForeignKey(Schueler, on_delete=models.CASCADE)
-
-
 class Raum_Belegung(models.Model):
     tablet_id = models.BigIntegerField()
     raum = models.ForeignKey(Raum, on_delete=models.CASCADE)
-    aufsichtsperson = models.ForeignKey(Personal, on_delete=models.CASCADE)  # in Raum_Belegung oder AG oder beides?
+    aufsichtsperson = models.ForeignKey(Personal, on_delete=models.CASCADE)             # in Raum_Belegung oder AG oder beides?
     angebots_zeit = models.ForeignKey(Zeitraum, on_delete=models.CASCADE)
     angebots_zeitraum = models.ForeignKey(Datumsraum, on_delete=models.CASCADE)
     ag = models.ForeignKey(AG, on_delete=models.CASCADE)
-
-
-class Aufenthalt(models.Model):  # Zuordnung wo sich Kinder befunden haben, wird mit löschen des Zeitraums auch gelöscht
+class Aufenthalt(models.Model):                # Zuordnung wo sich Kinder befunden haben, wird mit löschen des Zeitraums auch gelöscht
     tag = models.DateField
     schueler_id = models.ForeignKey(Schueler, on_delete=models.CASCADE)
     raum_id = models.ForeignKey(Raum, on_delete=models.CASCADE)
