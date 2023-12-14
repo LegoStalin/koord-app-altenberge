addd8e7db933e644d11ff8c45ef1409ef2043d5c
christiank119 <christian.kamann119@gmail.com>
2023-12-14 18:32:55 +0100

    Clean Branch + Fix
    Clean Branch for merge
    Fix Urls

--- koordapp/main_app/urls.py
+++ koordapp/main_app/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     path('master_web/', views.MasterHomeView.as_view(), name='master_web'),
     path('choose_room/', views.choose_Room, name='choose_room'),
-    path('choose_room/<int:raum_nr>', views.choose_RoomNr, name='choose_room_nr'),
+    path('create_activity/<int:raum_nr>', views.choose_Room, name='create_activity'),  # TODO Methode anpassen!!
 ]
--- koordapp/main_app/view/choose_room/choose_roomNr_view.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import redirect, render
-from main_app.models import Raum, Raum_Belegung
-
-def choose_roomNr_view(request, raum_nr):
-
-    return render(request, "choose_room/create_activity.html")
