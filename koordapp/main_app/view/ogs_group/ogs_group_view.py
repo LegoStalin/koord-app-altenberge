23fa54750138e0af50115ca052fbf1db3d4dd538
christiank119 <christian.kamann119@gmail.com>
2023-12-27 17:21:49 +0100

    Fix

--- koordapp/main_app/view/ogs_group/ogs_group_view.py
+++ koordapp/main_app/view/ogs_group/ogs_group_view.py
@@ -9,8 +9,7 @@ def ogs_group_view(request, gruppe):
     gruppe = Gruppe.objects.get(name=gruppe)
     if not gruppe == None:
         schueler = Schueler.objects.filter(gruppen_id=gruppe)
-        user = request.user()
-        user = User.objects.get("")
+        user = request.user
         personal = Personal.objects.get(user=user)
         if not Personal == None:
             if gruppe.gruppenleiter_leiter == personal:
