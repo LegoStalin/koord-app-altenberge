027aecbd6abf15dfc6e157ccbd7011377bacad50
Ericasch1 <epeters@uni-muenster.de>
2024-01-03 12:29:20 +0100

    Fehlermeldungen hinzugefügt

--- koordapp/main_app/view/create_activity/create_activity_view.py
+++ koordapp/main_app/view/create_activity/create_activity_view.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from main_app.models import Raum, Raum_Belegung, Personal, AGKategorie, AG
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group, User
+from django.contrib import messages
 
 def create_activity_view(request, raum):
     if(Raum.objects.filter(raum_nr=raum).exists()):
@@ -14,21 +15,24 @@ def create_activity_view(request, raum):
                     try:
                         max_anzahl = int(max_anzahl)
                         name_ag_kategorie = request.POST[""]
-                        if(AGKategorie.objects.filter(name=name_ag_kategorie).exists):
+                        if AGKategorie.objects.filter(name=name_ag_kategorie).exists():
                             ag_kategorie = AGKategorie.objects.get(name=name_ag_kategorie)
                             name_activity = request.POST["activity"]
-                            if(AG.objects.filter(name=name_activity).exists()):
+                            if AG.objects.filter(name=name_activity).exists():
                                 ag = AG.objects.get(name=name_activity)
                                 ag.ag_kategorie = ag_kategorie
                                 ag.max_anzahl = max_anzahl
                                 ag.save()
                             else:
+                                
                                 pass 
                         else:
                             # Error message wen kategorie nicht existiert
+                            messages.error(request, 'AG Kategorie existiert nicht')
                             pass
-                    except:
+                    except ValueError:
                         #error message wenn capazitaet keine Zahl ist!
+                        messages.error(request, 'Ungültige kapazitätsanzahl')
                         pass
             gruppenleiter_gruppe = Group.objects.get(name="Gruppenleitung")
             raumbetreuer_gruppe = Group.objects.get(name="Raumbetreuer")
