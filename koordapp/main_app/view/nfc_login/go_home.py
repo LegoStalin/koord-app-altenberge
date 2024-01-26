99414b1c9a982bd4befff6de153ea145983d90fa
christiank119 <christian.kamann119@gmail.com>
2024-01-26 17:06:00 +0100

    Update go_home.py

--- koordapp/main_app/view/nfc_login/go_home.py
+++ koordapp/main_app/view/nfc_login/go_home.py
@@ -4,10 +4,17 @@ from datetime import datetime
 
 def go_home_view(request, feedback=None):
     tag_id = request.session.get('tag_id', None)
+    last_tag_id = request.session.get('last_tag_id', None)
     if not tag_id == None:
         if(Nutzer.objects.filter(tag_id=tag_id).exists()):
             nutzer = Nutzer.objects.get(tag_id=tag_id)
-            if not feedback == None:
+            request.session['last_tag_id'] = tag_id
+            request.session['tag_id'] = None
+            return render(request, 'go_home/go_home.html', {"nutzer":nutzer})
+    elif not last_tag_id == None:
+        if not feedback == None:
+            if(Nutzer.objects.filter(tag_id=last_tag_id).exists()):
+                nutzer = Nutzer.objects.get(tag_id=last_tag_id)
                 try:
                     feedback = Feedback.Feedbacks[feedback.upper()].value
                 except KeyError:
@@ -15,9 +22,6 @@ def go_home_view(request, feedback=None):
                 if(Schueler.objects.filter(user_id=nutzer).exists()):
                     schueler = Schueler.objects.get(user_id=nutzer)
                     feedback = Feedback.objects.create(feedback_wert = feedback, schueler_id = schueler, tag = datetime.now().date(), zeit=datetime.now().time())
-                    print("Feedback für " + nutzer.vorname + " wurde gesetzt: " + feedback.feedback_wert)     #Debugging
-                    request.session['tag_id'] = None
+                    request.session['last_tag_id'] = None
                     redirect("home")
-            else:
-                return render(request, 'checked_out/checked_out.html', {"nutzer":nutzer})
     return redirect("home")
