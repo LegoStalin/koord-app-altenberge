33bb2ee1ab16e502c74d353fc415a15719472eda
christiank119 <christian.kamann119@gmail.com>
2024-02-07 18:44:18 +0100

    Food history

--- koordapp/main_app/view/history_pages_students/food_history.py
+++ koordapp/main_app/view/history_pages_students/food_history.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from main_app.models import Raum_Belegung, Aufenthalt, Nutzer, Schueler, Zeitraum, Feedback
-from datetime import datetime
+from datetime import datetime, timedelta
 
 @login_required(redirect_field_name="login")
 def food_history_view(request, pupil):
@@ -12,6 +12,21 @@ def food_history_view(request, pupil):
             feedbacks = Feedback.objects.filter(schueler_id=schueler, mensa_feedback=True)
             
             # get list if dates for the last to weeks
-            # lookup feedbacks contains an element where feedback.tag == date
 
-    return render(request, "history_pages/food_history.html", {'pupil':pupil})
+            hist = []
+            for i in range(14):
+                
+                datum = datetime.today() - timedelta(days=i)
+                if(feedbacks.filter(tag=datum).exists()):
+                    hist.append(Tup(date=datum.strftime("%d.%m.%y"), feed="Ja"))
+                else:
+                    hist.append(Tup(date=datum.strftime("%d.%m.%y"), feed="Nein"))
+            # lookup feedbacks contains an element where feedback.tag == date
+            
+
+    return render(request, "history_pages/food_history.html", {'pupil':pupil, "hist":hist})
+
+class Tup:
+    def __init__(self, date, feed):
+        self.date = date
+        self.feed = feed
